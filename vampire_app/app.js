e6e7db71d6b0f44fa1727959112579bd826d4921
Daniel Becker <danielbecker@MacBook-Pro.local>
2018-10-12 06:12:49 -0600

    Commit 2 - added data into vampire collection

--- vampire_app/app.js
+++ vampire_app/app.js
@@ -1,9 +1,10 @@
 // 1. Require your node modules
+const mongoose = require('mongoose');
 const connectionString = 'mongodb://localhost/test'
 mongoose.connect(connectionString);
 const Vampire = require('../model/vampire')
 // 2. Require your model (and possibly your extra data source);
-
+const vampireData = require('./populateVampires')
 // 3. Connect your database and collection name
 
 // 4. Open your mongoose connection
@@ -16,6 +17,99 @@ const Vampire = require('../model/vampire')
 /////////////////////////////////////////////////
 // INSERT USING MONGOOSE
 // ### Add the vampire data that we gave you
+
+// Vampire.collection.insertMany(vampireData,() => {
+//     console.log(vampireData)
+//     mongoose.connection.close();
+//   });
+
+// Vampire.create(
+//     { name: 'Steve',
+//     hair_color: 'Blue',
+//     eye_color: 'Green',
+//     dob: new Date(1971, 2, 13, 7, 47),
+//     loves: ['Food', 'Blood', 'Long walks on the beach'],
+//     location: 'Pensicola',
+//     gender: 'He/Him/His',
+//     victims: 4, 
+// }, (err, createdVampire) => {
+//     if(err){
+//         console.log(err)
+//     } else {
+//         console.log(createdVampire)
+//     }
+// }
+// );
+
+// Vampire.create(
+//     { name: '',
+//     hair_color: 'Blue',
+//     eye_color: 'Green',
+//     dob: new Date(1971, 2, 13, 7, 47),
+//     loves: ['Food', 'Blood', 'Long walks on the beach'],
+//     location: 'Pensicola',
+//     gender: 'He/Him/His',
+//     victims: 4, 
+// }, (err, createdVampire) => {
+//     if(err){
+//         console.log(err)
+//     } else {
+//         console.log(createdVampire)
+//     }
+// }
+// );
+// Vampire.create(
+//     { name: 'Lisa',
+//     hair_color: 'Blond',
+//     eye_color: 'Green',
+//     dob: new Date(1993, 2, 13, 7, 47),
+//     loves: ['Insta', 'PSLs', 'Ugg Boots', 'The blood of the innocent'],
+//     location: 'Chi O till I die O!!',
+//     gender: 'F',
+//     victims: 13, 
+// }, (err, createdVampire) => {
+//     if(err){
+//         console.log(err)
+//     } else {
+//         console.log(createdVampire)
+//     }
+// }
+// );
+// Vampire.create(
+//     { name: 'Frank',
+//     hair_color: 'Balding',
+//     eye_color: 'Brown',
+//     dob: new Date(1958, 2, 13, 7, 47),
+//     loves: ['Purell', 'Rum Ham'],
+//     location: 'Philadelphia',
+//     gender: 'M',
+//     victims: 100, 
+// }, (err, createdVampire) => {
+//     if(err){
+//         console.log(err)
+//     } else {
+//         console.log(createdVampire)
+//     }
+// }
+// );
+Vampire.create(
+    { name: 'Debbi',
+    hair_color: 'Brown',
+    eye_color: 'Green',
+    dob: new Date(1973, 2, 13, 7, 47),
+    loves: ['One Prozac a day', 'Springsteen', 'Madonna', 'U2', 'Blondie'],
+    location: 'Philadelphia',
+    gender: 'F',
+    victims: 1985, 
+}, (err, createdVampire) => {
+    if(err){
+        console.log(err)
+    } else {
+        console.log(createdVampire)
+    }
+}
+);
+
 
 // ### Add some new vampire data
 
@@ -59,7 +153,7 @@ const Vampire = require('../model/vampire')
 //## Negative Selection
 
 /////////////////////////////////////////////////
-const mongoose = require('mongoose');
+
 
 
 // listeners
